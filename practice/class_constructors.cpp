df046eb0de864706f18c6a3458cc4619818a74b0
Jatin <jatinrohilla69@gmail.com>
2017-11-25 03:55:11 +0530

    basics

---
+++ practice/class_constructors.cpp
@@ -0,0 +1,40 @@
+/*
+ * Author : Jatin Rohilla
+ * Date   :
+ *
+ * Editor   : Dev c++ 5.11
+ * Compiler : g++ 5.1.0
+ *
+ */
+
+
+#include<iostream>
+using namespace std;
+
+class A{
+    public:
+    A(){
+        cout<<"make A\n";
+    }
+    ~A(){
+        cout<<"destroy A\n";
+    }
+};
+
+class B: public A{
+    public:
+    B(){
+        cout<<"make B\n";
+    }
+    ~B(){
+        cout<<"destroy B\n";
+    }
+};
+
+int main(){
+
+    B b;
+
+    return 0;
+}
+
---
+++ practice/stdcpp_include.cpp
@@ -0,0 +1,21 @@
+/*
+ * Author : Jatin Rohilla
+ *
+ * Editor   : Dev c++ 5.11
+ * Compiler : g++ 5.1.0
+ *
+ * Super headers - can be pre compiled to save time.
+ */
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	cout<<"Hello World\n";
+	cout<<"This program uses the Non standard bits/stdc++.h ";
+
+    return 0;
+}
+
